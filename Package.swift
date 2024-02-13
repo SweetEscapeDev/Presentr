bdf935c0877f6e6befb995fff652e46ca1dd734c
yagrariksa <luckyrama0@gmail.com>
2024-02-13 11:55:31 +0700

    add SPM Availability
    Signed-off-by: yagrariksa <luckyrama0@gmail.com>

---
+++ Package.swift
@@ -0,0 +1,45 @@
+//  swift-tools-version:5.4
+//
+//  Package.swift
+//  Presentr
+//
+//  Created by Daffa Yagrariksa on 13/02/24.
+//  Copyright © 2024 danielozano. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Presentr",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Presentr",
+            targets: ["Presentr"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        .target(
+            name: "Presentr",
+            path: "Presentr",
+            exclude: [
+                "AlertViewController.xib",
+                "Info.plist",
+                "Montserrat-Regular.ttf",
+                "Presentr.h",
+                "SourceSansPro-Regular.ttf"
+            ],
+            resources: [
+                .copy("AlertViewController.xib"),
+                .copy("Montserrat-Regular.ttf"),
+                .copy("SourceSansPro-Regular.ttf")
+            ]
+        )
+    ]
+)
+
